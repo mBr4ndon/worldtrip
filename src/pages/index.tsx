bb252d85dc928e623e4cd20fdda2f583741ef7c3
Márcio Brandão <brandao.marcio.xix@gmail.com>
2021-09-27 16:56:19 +0100

    Finalização da página Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Heading, Flex, Text } from "@chakra-ui/react";
 import { Banner } from "../components/Banner";
 import { Header } from "../components/Header";
 import { Values } from "../components/Values";
+import { Navigation, Pagination, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+
+
 
 export default function Home() {
   return (
@@ -23,10 +32,33 @@ export default function Home() {
           color="gray.600"
           lineHeight="54px"
           textAlign="center"
-          mt="54px"
+          mt="52px"
+          mb="52px"
         >
           Vamos nessa? <br/>Então escolha seu continente
         </Heading>
+
+        <Swiper
+          navigation
+          pagination
+          modules={[Navigation, Pagination, A11y]}
+        >
+          <SwiperSlide>
+            <Flex
+              bgImage="images/europe.png"
+              w="100%"
+              h="100%"
+              align="center"
+              justify="center"
+              flexDirection="column"
+            >
+              <Heading color="white" fontWeight="700" fontSize="48px">Europa</Heading>
+              <Text color="gray.100" mt="4" fontWeight="700">O continente mais antigo</Text>
+            </Flex>
+          </SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+        </Swiper>
       </Box>
     </Box>
   )
